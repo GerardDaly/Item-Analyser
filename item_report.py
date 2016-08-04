ccc58e56aa6c771931a3744ad15d48cad21cdeee
Gerard Daly <GerardTDaly@gmail.com>
2016-08-04 00:35:07 -0400

    Clean by dirtying
    Some reason decided to take a script and turn it into a function. Now I
    have added more code than should be there.

--- item_report.py
+++ item_report.py
@@ -1,65 +1,111 @@
 # -*- coding: utf-8 -*-
-"""
-Created on Wed Aug 03 01:50:09 2016
 
-@author: Gerard
-"""
 
 import pandas as pd
 import matplotlib.pyplot as plt
 import numpy as np
+import datetime as dt
 
+
+    
 inv_tran = pd.read_csv('inv_tran.csv')
 po_tran = pd.read_csv('po_tran.csv')
 adj_tran = pd.read_csv('adj_tran.csv')
 cycle_tran = pd.read_csv('cycle_tran.csv')
 sales_tran = pd.read_csv('sales_tran.csv')
-
-def basic_stats(item_num):
-    #filters out item_num
-    item_tran = sales_tran[sales_tran['ITEM_NUM'] == item_num]
-    #calculate and prints mean and standard deviation - should turn to an object
-    mean = item_tran['SALES'].mean()
-    std= item_tran['SALES'].std() 
-    print 'The average sale of the item is', mean
-    print 'The standard deviation of the item is', std
-    #plots daily use plots single purchases.
-    item_tran['SALES'].hist(bins = 50)
-    day_group = item_tran['SALES'].groupby(item_tran['INV_DATE'])
-    by_day = day_group.agg(np.sum)    
-    by_day.hist(bins = 50)
-    daily_mean = by_day.mean()
-    daily_std = by_day.std()
-    print 'The average daily throughput is' , daily_mean
-    print 'The daily throughput standard deviation is', daily_std
-    cust_group = item_tran['SALES'].groupby(item_tran['CUST_NAME'])
-    by_cust = cust_group.agg(np.sum).nlargest(5)
-    sum_sales = item_tran['SALES'].sum()
-    by_cust = pd.DataFrame(by_cust)
-    by_cust['PERC_OF_ITEM'] = by_cust['SALES'].apply(lambda x: (x/sum_sales)*100)
-    print by_cust
-    return by_cust
-
-#item = basic_stats('ATCS24AWH')
-
-def basic_cycle(item_num):
-    item_filter = cycle_tran['ITEM_NUM'] == item_num
-    item_tran = cycle_tran[item_filter]
-    day_group = item_tran['ADJUSTMENTS'].groupby(item_tran['INV_DATE']).agg(np.sum)
     
-    return day_group
+    
+def basic_filter(item_num, csvfile):
+    item_filter = csvfile['ITEM_NUM'] == item_num.upper()
+    item_tran = csvfile[item_filter]
+    return item_tran
+    
+def item_desc(item_sales):
+    item_desc = item_sales.iloc[1]['ITEM_DESC']
+    return item_desc
+    
+def basic_stats(series):
+    mean = series.mean()
+    std = series.std()
+    basic_stats = {'mean':mean, 'std':std}
+    return basic_stats
 
-cycle = basic_cycle('02GASTP3CH')
-print cycle.head(5)
+def plots(hist1, hist2, day_plot, month_plot):
+    plt.figure()
+    hist1.hist(bins = 50)
+    plt.figure()
+    hist2.hist(bins = 50)
+    plt.figure()
+    day_plot.plot()
+    plt.figure()
+    month_plot.plot()
+    
+def sales_info(item_num):
+    item_sales = basic_filter(item_num, sales_tran)
+    item_descr = item_desc(item_sales)
+    sale_stats = basic_stats(item_sales['SALES'])
+    day_sales = item_sales['SALES'].groupby(item_sales['INV_DATE']).agg(np.sum)
+    day_stats = basic_stats(day_sales)
+        
+    #Groups sales by customer and takes the 5 largest. Gives percentage of total sales for those.
+    cust_group = item_sales['SALES'].groupby(item_sales['CUST_NAME']).agg(np.sum).nlargest(5)
+    cust_group = pd.DataFrame(cust_group)
+    sum_sales = item_sales['SALES'].sum()
+    cust_group['PERC_OF_ITEM'] = cust_group['SALES'].apply(lambda x: (x/sum_sales)*100)
+    
+    #Dictionary with all relevant information from sales_info.
+    sales_data = {'sales_mean':sale_stats['mean'], 'sales_std': sale_stats['std'],
+                  'day_mean':day_stats['mean'], 'day_std': day_stats['std'], 'top_5': cust_group,
+                  'item_desc': item_descr, 'item_num':item_num}               
+    return sales_data
 
+def cycle_info(item_num):
+    item_cycle = basic_filter(item_num, cycle_tran)
+    #aggregating by day makes more sense, avoids same day errors in input.
+    day_cycle = item_cycle['ADJUSTMENTS'].groupby(item_cycle['INV_DATE']).agg(np.sum)
+    
+    cycle_stats = basic_stats(day_cycle)
+    
+    # Converts datetimes to calculate last cycle count.
+    day_cycle.index = pd.to_datetime(day_cycle.index)
+    recent_date = day_cycle.index.max()
+    recent_date = recent_date.date()
+    time_since = dt.date.today() - recent_date
+    
+    # Dictionary with all relelevant cycle info.
+    cycle_data = {'cycle_mean':cycle_stats['mean'], 'cycle_std':cycle_stats['std'],
+                  'recent_date':recent_date, 'time_since': time_since}
+    return cycle_data
+    
+
+def faux_report(sales_data, cycle_data):   
+    print 'Item Number:{0}, aka: {1}.'.format(sales_data['item_num'],sales_data['item_desc'])
+    print 'The item has a purchase mean of {0} and a standard deviation of {1}.'.format(sales_data['sales_mean'],sales_data['sales_std'])
+    print 'Per day mean of {0} and standard deviation of {1}.'.format(sales_data['day_mean'],sales_data['day_std'])
+    print 'The top 5 customers, with percentage of sales are:', sales_data['top_5']
+    print 'The average variance on a cycle count is {0} with a standard devation of {1}'.format(cycle_data['cycle_mean'],cycle_data['cycle_std'])
+    print 'The last cycle count occured {0}, roughly {1} days ago.' .format(cycle_data['recent_date'], cycle_data['time_since'])
+    
+item_number = raw_input ('What is the item number you are looking for? ')
+
+s_data = sales_info(item_number)
+c_data = cycle_info(item_number)
+
+print faux_report(s_data, c_data)
+    
 
 '''
-Put an upper function on item numbers and error handling when item doesn't exist.
-Move prints to some sort of function - the variables are sent to a class.
-Print Item Description at the top.
-Plot of current stock next to sales.
-Bins need to change depending on amount usually purchased (mean).
-Simple report of last couple of cycles.
-Import the correct types for each variable in the other module.
-Need to figure out how to let the user select the file through Windows.
+by day
+  std
+  mean
+by sale
+  std
+  mean
+
+hist by sale
+hist by day
+
+plot by day
+plot by months
+
 '''
